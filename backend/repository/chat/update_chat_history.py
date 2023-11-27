ffb056edf5e21704e396ac4b2d5f068451248c5d
FernandoSLuz <fernandosilvadaluz@gmail.com>
2023-11-26 20:32:51 -0500

    update chat last_update

--- backend/repository/chat/update_chat_history.py
+++ backend/repository/chat/update_chat_history.py
@@ -12,4 +12,12 @@ def update_chat_history(chat_history: CreateChatHistory) -> ChatHistory:
         raise HTTPException(
             status_code=500, detail="An exception occurred while updating chat history."
         )
+    else:
+        #update chats row with last_update timestamp from response
+        chat_response = supabase_db.update_chat(chat_history.chat_id, {"last_update": ChatHistory(response[0]).message_time})
+        if len(chat_response.data) == 0:
+            raise HTTPException(
+                status_code=500, detail="An exception occurred while updating chat history."
+            )
+
     return ChatHistory(response[0])  # pyright: ignore reportPrivateUsage=none
